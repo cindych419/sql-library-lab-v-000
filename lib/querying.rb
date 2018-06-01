9e20307c816e22da6005c2364f04190035c00e44
cindych419@gmail.com <cindych419@gmail.com>
2018-06-01 22:03:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,9 +12,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species)
-  FROM characters
-  ORDER BY COUNT(characters.species) ASC LIMIT 1;"
+  "SELECT characters.species, MAX(COUNT(characters.species))
+  FROM characters;"
 end
 
 def select_name_and_series_subgenres_of_authors
