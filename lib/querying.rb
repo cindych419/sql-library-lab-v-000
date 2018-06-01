c3e0023fcbaeebd3e439862d61587b7386c59c58
cindych419@gmail.com <cindych419@gmail.com>
2018-06-01 22:20:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors, subgenres
+  where authors.id = series.author_id, subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
