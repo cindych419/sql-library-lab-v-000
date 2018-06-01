6cd15b86f1462657d4874030878bbbceab0f328f
cindych419@gmail.com <cindych419@gmail.com>
2018-06-01 21:26:27 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,3 +8,13 @@ CREATE TABLE subgenres(
   id INTEGER PRIMARY KEY,
   name TEXT
 );
+CREATE TABLE authors(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+CREATE TABLE books(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
